cbe656566d0817bee9feb87d0c7016db4c119b71
Glyn Normington <glyn@underlap.org>
2026-01-06 15:51:06 +0000

    Panic while handling "sending" message
    Add a test to reproduce the following bug:
    
    https://github.com/glyn/ipc-channel-mux/issues/3

--- tests/mux_integration_test.rs
+++ tests/mux_integration_test.rs
@@ -60,3 +60,43 @@ fn spawn_sub_one_shot_server_client() {
         result.code().expect("exit status code not available")
     );
 }
+
+/// Test behaviour when a SubSender is sent to a process which
+/// terminates before the "sending" message has been received.
+#[test]
+#[ignore = "https://github.com/glyn/ipc-channel-mux/issues/3"]
+fn subsender_drop_inflight_early() {
+    let executable_path: String = env!("CARGO_BIN_EXE_crashing_receiving_process").to_string();
+
+    type ChannelPair = (mux::SubSender<mux::SubSender<bool>>, mux::SubSender<()>);
+
+    let (server, token) =
+        mux::SubOneShotServer::<ChannelPair>::new().expect("Failed to create sub one-shot server");
+
+    let mut command = process::Command::new(executable_path);
+    let child_process = command.arg(token);
+
+    let mut child = child_process
+        .spawn()
+        .expect("Failed to start child process");
+
+    let (_rx, (data, control)): (mux::SubReceiver<ChannelPair>, ChannelPair) =
+        server.accept().expect("accept failed");
+
+    let d = mux::Channel::new().unwrap();
+    let (transmit_tx, transmit_rx) = d.sub_channel();
+
+    data.send(transmit_tx).expect("subsender send failed");
+    control.send(()).expect("control send failed");
+    let result = child.wait().expect("wait for child process failed");
+    assert_eq!(
+        result.code().unwrap(),
+        1,
+        "child process did not terminate with exit status code 1"
+    );
+
+    match transmit_rx.recv() {
+        Err(mux::MultiplexError::Disconnected) => {},
+        result => panic!("unexpected result {:?}", result),
+    }
+}
