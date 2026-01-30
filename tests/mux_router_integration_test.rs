2b80f9639d55e3bdc6f5698c06fe788d174bea85
Glyn Normington <glyn@underlap.org>
2026-01-30 16:57:12 +0000

    WIP: Rework subsending drop integration tests
    The following sometimes hangs:
    
    RUST_LOG=trace cargo test subsender_drop_inflight_subreceiver_receiving -- --exact --nocapture
    
    See for example scratch/hang.log.

--- tests/mux_router_integration_test.rs
+++ tests/mux_router_integration_test.rs
@@ -8,60 +8,12 @@
 // except according to those terms.
 
 use ipc_channel_mux::mux::subchannel_router::{ROUTER, RouterProxy};
-use ipc_channel_mux::mux::{Channel, MuxError, SubOneShotServer, SubReceiver, SubSender};
+use ipc_channel_mux::mux::{SubOneShotServer, SubReceiver, SubSender};
 use std::{env, process};
 use test_log::test;
 
 // The integration tests may be run on their own by issuing:
 // cargo test --test '*'
-
-/// Test non-router polling behaviour when a SubSender is sent to a
-/// process which terminates before the SubSender has been
-/// received.
-///
-/// This provides the basis for testing the router behaviour.
-#[test]
-fn subsender_drop_inflight() {
-    let executable_path: String = env!("CARGO_BIN_EXE_crashing_receiving_process").to_string();
-
-    type ChannelPair = (SubSender<SubSender<bool>>, SubSender<()>);
-
-    let (server, token) =
-        SubOneShotServer::<ChannelPair>::new().expect("Failed to create sub one-shot server");
-
-    let mut command = process::Command::new(executable_path);
-    let child_process = command.arg(token);
-
-    let mut child = child_process
-        .spawn()
-        .expect("Failed to start child process");
-
-    let (_rx, (data, control)): (SubReceiver<ChannelPair>, ChannelPair) =
-        server.accept().expect("accept failed");
-
-    let d = Channel::new().unwrap();
-    let (transmit_tx, transmit_rx) = d.sub_channel();
-
-    data.send(transmit_tx).expect("subsender send failed");
-
-    // Ensure that the "sending" message is received so that polling occurs.
-    let (extra_tx, extra_rx) = d.sub_channel();
-    extra_tx.send(()).unwrap();
-    extra_rx.recv().unwrap(); // uses the same IpcReceiver as transmit_rx
-
-    control.send(()).expect("control send failed");
-    let result = child.wait().expect("wait for child process failed");
-    assert_eq!(
-        result.code().unwrap(),
-        1,
-        "child process did not terminate with exit status code 1"
-    );
-
-    match transmit_rx.recv() {
-        Err(MuxError::Disconnected) => {},
-        result => panic!("unexpected result {:?}", result),
-    }
-}
 
 /// Test router behaviour when a SubSender is sent to a
 /// process which terminates before the SubSender has been
