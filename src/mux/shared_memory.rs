43ba4858682dd663967806e4199ddada3970042e
Glyn Normington <glyn@underlap.org>
2026-02-25 11:50:02 +0000

    Update copyright

--- src/mux/shared_memory.rs
+++ src/mux/shared_memory.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 The Servo Project Developers. See the COPYRIGHT
+// Copyright 2026 The Servo Project Developers. See the COPYRIGHT
 // file at the top-level directory of this distribution.
 //
 // Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
