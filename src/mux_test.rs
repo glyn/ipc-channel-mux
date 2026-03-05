376db52205051ed089cdf584507127131bd966be
Glyn Normington <glyn@underlap.org>
2026-03-05 03:30:54 +0000

    Address TODO

--- src/mux_test.rs
+++ src/mux_test.rs
@@ -224,13 +224,16 @@ fn receiving_many_subchannels() {
 
         let mut senders = vec![];
         loop {
-            let send2 = recv1.recv().unwrap(); // TODO: at the end of the test, this panics. Better if the thread terminated gracefully.
-            send2.send(true).unwrap();
-            // The fd is private, but this transmute lets us get at it
-            let fd: &std::sync::Arc<u32> = unsafe { std::mem::transmute(&send2) };
-            println!("fd = {}", *fd);
-            // Stop the ipc channel from being dropped
-            senders.push(send2);
+            if let Ok(send2) = recv1.recv() {
+                send2.send(true).unwrap();
+                // The fd is private, but this transmute lets us get at it
+                let fd: &std::sync::Arc<u32> = unsafe { std::mem::transmute(&send2) };
+                println!("fd = {}", *fd);
+                // Stop the ipc channel from being dropped
+                senders.push(send2);
+            } else {
+                return;
+            }
         }
     });
 
